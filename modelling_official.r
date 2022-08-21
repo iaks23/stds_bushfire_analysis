6c23f6ce656cd57544c350866bf80a58fcd9fbce
Akshaya Parthasarathy <akshaya.sarathy97@gmail.com>
2022-08-21 11:50:21 +1000

    initial setup

---
+++ modelling_official.r
@@ -0,0 +1,10 @@
+################ Clearing everything ################
+
+rm(list = ls())
+
+################ Loading Packages ################
+library(here)
+library(tidyverse) 
+library(ggplot2)
+library(janitor)
+library(dplyr)
